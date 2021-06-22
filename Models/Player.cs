8dd4a28bcff3c65afae480a3966876f66aa0d18f
Nathan Phame <diyf2021@gmail.com>
2021-06-22 02:06:03 -0400

    Added sample data and generated Player Controller

---
+++ Models/Player.cs
@@ -0,0 +1,14 @@
+namespace TennisBuds.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+        public string Court { get; set; }
+        public int Zip { get; set; }
+        public int Rating { get; set; }
+
+    }
+}
---
+++ Models/exampledata.sql
@@ -0,0 +1,6 @@
+TRUNCATE TABLE "Players";
+INSERT INTO "Players" ("Name", "Email", "Telephone", "Court", "Zip", "Rating") VALUES ('Alex Andre', 'aaa@gmail.com', '111-111-1111', 'Fossil Park', 33710, 4.0);
+INSERT INTO "Players" ("Name", "Email", "Telephone", "Court", "Zip", "Rating") VALUES ('Betty Benson', 'bbb@gmail.com', '222-222-2222', 'Lazarillo Park', 33711, 3.0);
+INSERT INTO "Players" ("Name", "Email", "Telephone", "Court", "Zip", "Rating") VALUES ('Carl Clark', 'ccc@gmail.com', '333-333-3333', 'Fossil Park', 33712, 5.0);
+INSERT INTO "Players" ("Name", "Email", "Telephone", "Court", "Zip", "Rating") VALUES ('Dennis Dent', 'ddd@gmail.com', '444-444-4444', 'Lakeside Parl', 33713, 3.5);
+
